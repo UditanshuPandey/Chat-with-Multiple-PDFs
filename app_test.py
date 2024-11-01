69901aa6372d55f35649e13ef63f00e298c3bef7
Uditanshu Pandey <75218958+UditanshuPandey@users.noreply.github.com>
2024-11-01 11:01:41 +0530

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -76,7 +76,7 @@ def main():
 
     with st.sidebar:
         st.subheader("Your documents")
-        pdf_docs = st.file_uploader("Upload your PDFs here and click on 'Process'", accept_ _files=True)
+        pdf_docs = st.file_uploader("Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
         if st.button("Process"):
             if pdf_docs:
                 with st.spinner("Processing..."):
